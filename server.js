100b9e22198c68adf339d1d6aa2f9572b6e2cb17
69319010400-rgb <69319010400@techno-chon.ac.th>
2026-07-27 22:02:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ client.release(); // คนืการเชื่อมตอเมื่�
 // 4. นําขอมูลที่ได(result.rows) มาประกอบเปนตาราง HTML
 let html = `<h1>ฐานขอมูลนักศึกษา (ทดสอบการเชื่อมตอ)</h1>`;
 html += `<table border="1" cellpadding="10">`;
-html += `<tr><th>รหัสนักศึกษา</th><th>ชื่อ-นามสกุล</th></tr>`;
+html += `<tr><th>69319010400</th><th>ลักษิกา เกตุศรีแก้ว</th></tr>`;
 // วนลูปนําขอมูลแตละแถวมาแสดง
 result.rows.forEach(row => {
 html += `<tr><td>${row.student_id}</td><td>${row.student_name}</td></tr>`;
@@ -28,7 +28,7 @@ res.end(html);
 } catch (err) {
 // กรณเีชื่อมตอไมไดหรือเขียนชื่อตารางผิด
 console.error(err);
-res.end(`<h1>เกิดขอผิดพลาด!</h1><p>${err.message}</p>`);
+res.end(`<h1>เกิดข้อผิดพลาด!</h1><p>${err.message}</p>`);
 }
 });
 server.listen(port, () => {
