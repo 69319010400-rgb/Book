a16e8c442cca35b57dcae4c72226a7250186030d
69319010400-rgb <69319010400@techno-chon.ac.th>
2026-07-27 22:46:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Pool } = require('pg');
 // 2. ตั้งคาการเชื่อมตอ โดยดึง URL มาจาก Environment Variable ของ Railway
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false
   }
